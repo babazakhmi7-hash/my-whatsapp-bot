7df30731122c9e7b41f99365c23638792256d9d9
babazakhmi7-hash <babazakhmi7@gmail.com>
2026-08-02 13:53:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const pino = require('pino');
-const qrcode = require('qrcode-terminal');
 
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
     
     const sock = makeWASocket({
         logger: pino({ level: 'silent' }),
-        auth: state
+        auth: state,
+        printQRInTerminal: false
     });
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect, qr } = update;
-        
-        if (qr) {
-            console.log('Scan this QR Code:');
-            qrcode.generate(qr, { small: true });
-        }
+    const phoneNumber = "923336368652"; 
 
+    if (!sock.authState.creds.registered) {
+        setTimeout(async () => {
+            let code = await sock.requestPairingCode(phoneNumber);
+            console.log(`\n========================================`);
+            console.log(`YOUR PAIRING CODE IS: ${code}`);
+            console.log(`========================================\n`);
+        }, 3000);
+    }
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
         if (connection === 'close') {
-            console.log('Connection closed, reconnecting...');
-            startBot();
+            const shouldReconnect = (lastDisconnect?.error)?.output?.statusCode !== DisconnectReason.loggedOut;
+            console.log('Connection closed due to ', lastDisconnect?.error, ', reconnecting ', shouldReconnect);
+            if (shouldReconnect) {
+                startBot();
+            }
         } else if (connection === 'open') {
             console.log('Bot connected successfully!');
         }
