0606229cdf4b4ccba25521c3f6d588d3ff341a56
babazakhmi7-hash <babazakhmi7@gmail.com>
2026-08-02 13:13:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-{
-  "name": "my-bot",
-  "version": "1.0.0",
-  "type": "module",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "@whiskeysockets/baileys": "^6.7.5",
-    "pino": "^8.19.0",
-    "qrcode-terminal": "^0.12.0",
-    "yt-search": "^2.10.4"
-  }
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
+const pino = require('pino');
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
+    
+    const sock = makeWASocket({
+        logger: pino({ level: 'silent' }),
+        auth: state
+    });
+
+    sock.udarstven = 'connection.update';
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            console.log('Connection closed, reconnecting...');
+            startBot();
+        } else if (connection === 'open') {
+            console.log('Bot connected successfully!');
+        }
+    });
+
+    sock.ev.on('creds.update', saveCreds);
 }
+
+startBot();
