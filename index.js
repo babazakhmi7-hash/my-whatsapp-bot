cc34f7dcf465f3370a32ce273cf0faa4a820e8f1
babazakhmi7-hash <babazakhmi7@gmail.com>
2026-08-02 13:34:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const pino = require('pino');
+const qrcode = require('qrcode-terminal');
 
 async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
@@ -9,9 +10,14 @@ async function startBot() {
         auth: state
     });
 
-    sock.udarstven = 'connection.update';
     sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update;
+        const { connection, lastDisconnect, qr } = update;
+        
+        if (qr) {
+            console.log('Scan this QR Code:');
+            qrcode.generate(qr, { small: true });
+        }
+
         if (connection === 'close') {
             console.log('Connection closed, reconnecting...');
             startBot();
